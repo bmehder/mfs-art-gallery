5e1e7371d98368562030290e1b155844c21cb147
bmehder <bmehder@gmail.com>
2022-02-03 13:45:05 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Card from './Card.svelte'
+
+  const endpoint =
+    'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/?category=art-class'
+
+  let posts = []
+
+  const getPosts = async node => {
+    const res = await fetch(endpoint)
+    const data = await res.json()
+
+    posts = data.posts
+  }
+</script>
+
+<aside use:getPosts>
+  {#each posts as { featured_image, excerpt }, index}
+    <Card src={featured_image} {excerpt} {index} />
+  {/each}
+</aside>
+
+<style>
+  aside {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+</style>
---
+++ src/Card.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let src = ''
+  export let excerpt = ''
+  export let index = 0
+
+  let isBig = false
+  let innerWidthValue = 0
+
+  const handlClick = e => {
+    !isBig &&
+      index % 2 !== 0 &&
+      window.scrollBy({
+        top: 600,
+        left: 0,
+        behavior: 'smooth',
+      })
+
+    isBig &&
+      index % 2 !== 0 &&
+      window.scrollBy({
+        top: -600,
+        left: 0,
+        behavior: 'smooth',
+      })
+
+    isBig = !isBig
+  }
+</script>
+
+<svelte:window bind:innerWidth={innerWidthValue} />
+
+<article class:big={isBig} on:click={innerWidthValue > 500 && handlClick}>
+  <img {src} alt="Card" />
+  <p>{@html excerpt}</p>
+</article>
+
+<style>
+  article {
+    flex-basis: 350px;
+    min-width: 350px;
+    margin-bottom: 1rem;
+    background: #f0f2f5;
+    border-radius: 1em;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.24);
+    overflow: hidden;
+    transition: all 500ms ease-in-out;
+  }
+  img {
+    display: block;
+    width: 100%;
+    height: 350px;
+    object-fit: cover;
+  }
+  p {
+    padding: 2rem;
+    line-height: 1.5rem;
+  }
+  .big {
+    flex-basis: 100%;
+  }
+  .big img {
+    width: auto;
+    height: 100vh;
+    margin: auto;
+  }
+</style>
